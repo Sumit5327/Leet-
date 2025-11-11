ba382ce80b9ed5353a3f94fe6c4bfc0c1000b5cb
Sumit5327 <trapasiyasumit53@gmail.com>
2025-11-11 15:01:44 +0530

    Time: 2072 ms (24.27%), Space: 7.7 MB (54.49%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int count = 0;
+        for(int i=1;i<num;i++)
+        {
+            if(num % i == 0)
+            {
+                count += i;
+            }
+        }
+        return num == count;
+    }
+};
